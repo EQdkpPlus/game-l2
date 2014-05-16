f199ffa6e85afb31dc1be09a586cfae24c64c103
godmod <godmod@eqdkp-plus.eu>
2014-05-16 15:58:20 +0000

    ported some games, except lotro and warhammer, they need some changes

--- l2.class.php
+++ l2.class.php
@@ -120,8 +120,8 @@ if(!class_exists('l2')) {
 		* @param bool $install
 		* @return array
 		*/
-		public function get_OnChangeInfos($install=false){
-			$info['aq'] = array();
+		public function install($install=false){
+			
 
 			//Do this SQL Query NOT if the Eqdkp is installed -> only @ the first install
 			#if($install){
