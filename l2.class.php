86f3f899d157688dac3496f4c3c9dbe7f578dd3f
wallenium <wallenium@eqdkp-plus.eu>
2014-09-18 20:36:44 +0000

    - and again... got rid of a lot of bugs

--- l2.class.php
+++ l2.class.php
@@ -107,7 +107,7 @@ if(!class_exists('l2')) {
 					'type'			=> 'text',
 					'category'		=> 'character',
 					'lang'			=> 'uc_guild',
-					'size'			=> 40,
+					'size'			=> 32,
 					'undeletable'	=> true,
 				),
 			);
